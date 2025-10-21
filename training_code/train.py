ac49189fefeffb4cc4c5404f929331f823ab16ee
Vedran <dikobrazman@hotmail.com>
2025-10-21 09:29:17 +0200

    .

--- training_code/train.py
+++ training_code/train.py
@@ -151,7 +151,7 @@ if __name__=="__main__":
     with open(os.path.join(save_dir, f"exp{exp_num}_info.txt"), "a") as f:
         f.write(f"start: {datetime.datetime.now()}\n")
 
-    train(model, train_dataloader, val_dataloader, param_niter=20, save_dir=save_dir, exp_num=exp_num, criterion=criterion)
+    train(model, train_dataloader, val_dataloader, param_niter=4, save_dir=save_dir, exp_num=exp_num, criterion=criterion)
 
 
     eval.evaluate(model, trainset, valset, save_dir, exp_num, criterion)
